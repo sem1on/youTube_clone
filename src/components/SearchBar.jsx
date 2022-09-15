399435a6ffa456ef200dfaf33e8301b4dd298540
sem1on <sgorlov21@gmail.com>
2022-09-15 19:43:51 +0300

    SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Paper, IcomButton, IconButton } from '@mui/material';
+import { Search } from '@mui/icons-material';
 
 const SearchBar = () => {
     return (
-        <div>
-            
-        </div>
+        <Paper
+            component='form'
+            sx={{ 
+                borderRadius: 20,
+                border: '1px solid #e3e3e3',
+                pl: 2,
+                boxShadow: 'none',
+                mr: { sm: 5 },
+            }}
+            onSubmit={() => {}}
+        >
+            <input 
+                className='search-bar'
+                placeholder='Search.....'
+                value=''
+                onChange={() => {}} 
+            />
+            <IconButton type='submit' sx={{ p: '10px' , color: 'red'}}>
+                <Search/>
+            </IconButton>
+        </Paper>
     );
 };
 
